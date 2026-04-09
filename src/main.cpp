2e71da5e9935d17fc7e66cd0ee77758daad393c4
Cubek <manak47real@gmail.com>
2026-04-09 23:37:11 +0300

    add $modify for load after loading assets

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 #include "openurl/searchtab.h"
+#include <Geode/loader/Dirs.hpp>
 #include <url.h>
 
 using namespace geode::prelude;
@@ -8,3 +9,10 @@ using namespace geode::prelude;
 $on_mod(Loaded) {
 	CustomUrl gdlink("gdlink");
 }
+class $modify(MenuLayer) {
+	bool init() {
+		if (!MenuLayer::init()) return false;
+		log::debug("success");
+		return true;
+	}
+};
