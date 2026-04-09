f799db0be91b2946aa13d363c302f428ece859d1
Cubek <manak47real@gmail.com>
2026-04-09 18:48:17 +0300

    launch notepad example

--- platform/windows/url.cpp
+++ platform/windows/url.cpp
@@ -4,4 +4,25 @@
 Notify::Notify() {}
 Notify::~Notify() {}
 
-void Notify::sendNotification(const char* title, const char* text) {}
+void Notify::sendNotification(const char* title, const char* text) {
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);  // ОБЯЗАТЕЛЬНО
+
+    ZeroMemory(&pi, sizeof(pi));
+
+    CreateProcess(
+        NULL,
+        (LPSTR)"notepad.exe",
+        NULL,
+        NULL,
+        FALSE,
+        0,
+        NULL,
+        NULL,
+        &si,
+        &pi
+    );
+}
