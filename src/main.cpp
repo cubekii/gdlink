972816884a7da09af4ce3feda74f9cf3a76830df
Cubek <manak47real@gmail.com>
2026-04-10 00:28:58 +0300

    add new part

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/LoadingLayer.hpp>
 #include "openurl/searchtab.h"
 #include <Geode/loader/Dirs.hpp>
 #include <url.h>
@@ -9,10 +10,12 @@ using namespace geode::prelude;
 $on_mod(Loaded) {
 	CustomUrl gdlink("gdlink");
 }
-class $modify(MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) return false;
-		log::debug("success");
-		return true;
+class $modify(LoadingLayer) {
+	void loadAssets() {
+		LoadingLayer::loadAssets(); // call original first
+
+		if (m_loadStep >= 14) { // 14 = last step in GD 2.2
+			gdloader::loadlevel(90475473);
+		}
 	}
 };
