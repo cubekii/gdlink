e7b1f906ea9ffd691ffd5c85af9c85a5b353d5a6
Cubek <manak47real@gmail.com>
2026-04-10 01:09:51 +0300

    it's working!

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,32 @@
 
 using namespace geode::prelude;
 
+std::optional<std::string> pendingUrl = std::nullopt;
+
 $on_mod(Loaded) {
-	CustomUrl gdlink("gdlink");
+	std::string cmdLine = GetCommandLineA();
+	log::info("Full command line: {}", cmdLine);
+
+	// find gdlink:// in it
+	size_t pos = cmdLine.find("gdlink://");
+	if (pos != std::string::npos) {
+		pendingUrl = cmdLine.substr(pos);
+		// trim trailing quote if present: "gdlink://level/42"
+		if (!pendingUrl->empty() && pendingUrl->back() == '"')
+			pendingUrl->pop_back();
+	}
 }
 class $modify(LoadingLayer) {
 	void loadAssets() {
 		LoadingLayer::loadAssets(); // call original first
 
 		if (m_loadStep >= 14) { // 14 = last step in GD 2.2
-			gdloader::loadlevel(90475473);
+			//gdloader::loadlevel(90475473);
+
+			if (!pendingUrl->empty())
+				Notification::create(pendingUrl->c_str())->show();
+			else
+				Notification::create("it's not working!")->show();
 		}
 	}
 };
