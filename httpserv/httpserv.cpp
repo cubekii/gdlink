519887e4a6242a8cff304d2b6f484794d46b5f32
Cubek <manak47real@gmail.com>
2026-04-10 15:26:19 +0300

    add simple httpserv

---
+++ httpserv/httpserv.cpp
@@ -0,0 +1,16 @@
+#include "httpserv.h"
+#include "../libs/cpp-httplib/httplib.h"
+
+inline void httpserv::startserver() {
+    httplib::Server svr;
+
+    svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content("Hello, World!", "text/plain");
+    });
+
+    svr.Get("/api/data", [](const httplib::Request& req, httplib::Response& res) {
+        res.set_content("{\"status\": \"ok\"}", "application/json");
+    });
+
+    svr.listen("localhost", 6767);
+}
---
+++ httpserv/httpserv.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "../libs/cpp-httplib/httplib.h"
+
+namespace httpserv {
+    inline void startserver();
+}
