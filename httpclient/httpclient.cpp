cc65d9940bb4ead4deb721974801901506e84503
Cubek <manak47real@gmail.com>
2026-04-12 16:22:21 +0300

    clean code

--- httpclient/httpclient.cpp
+++ httpclient/httpclient.cpp
@@ -1,8 +1,5 @@
 #include "httpclient.h"
 #include <httplib.h>
-#include <iostream>
-
-const std::string secret = "Wmfd2893gb7";
 
 void httpclient::fetch(const std::string& levelId) {
     httplib::Client cli("https://www.boomlings.com");
@@ -15,12 +12,12 @@ void httpclient::fetch(const std::string& levelId) {
     cli.set_read_timeout(10);
 
     httplib::Params params = {
-        { "gameVersion", "22"        },
-        { "binaryVersion", "42"      },
-        { "gdw",         "0"         },
-        { "str",         levelId     },  // the level ID to look up
-        { "type",        "19"        },  // 19 = search by level ID
-        { "secret",      secret      }
+        { "gameVersion", "22"         },
+        { "binaryVersion", "42"       },
+        { "gdw",         "0"          },
+        { "str",         levelId      },  // the level ID to look up
+        { "type",        "19"         },  // 19 = search by level ID
+        { "secret",      "Wmfd2893gb7"}
     };
 
     auto res = cli.Post("/database/getGJLevels21.php", params);
